c7c15df92771cc088f32d2d150844c0d2b105b0a
JsonAndrx <yeison.pev1@gmail.com>
2023-05-28 18:17:26 -0500

    adding sentry to render

--- buildpro/settings/production.py
+++ buildpro/settings/production.py
@@ -3,6 +3,8 @@ import os
 from buildpro.logging import *
 from dotenv import load_dotenv
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv(Path.joinpath(BASE_DIR, 'dotenv'))
 
@@ -17,6 +19,24 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+
+
+sentry_sdk.init(
+    dsn="https://ef260c4b1cc246cb969678b9e1eb5192@o4505250926362625.ingest.sentry.io/4505264163586048",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
